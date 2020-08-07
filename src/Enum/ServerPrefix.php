7de3016c30de1b6e9dce2158bad1ea2ce8262999
Ben Peachey <potherca@gmail.com>
2020-08-07 17:03:54 +0200

    Add enum class to hold the prefix used for each server type.

---
+++ src/Enum/ServerPrefix.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Pdsinterop\Authentication\Enum;
+
+class ServerPrefix
+{
+    public const AUTHORIZATION = '/oauth2';
+    public const CLIENT = '/client';
+    public const RESOURCE = '/resource';
+}
