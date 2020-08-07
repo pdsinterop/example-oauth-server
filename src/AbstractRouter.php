a7ebf2079331cfacb047e534626567cf3db20b7b
Ben Peachey <potherca@gmail.com>
2020-08-07 18:08:26 +0200

    Change the AbstractRouter to create responses for exceptions.

--- src/AbstractRouter.php
+++ src/AbstractRouter.php
@@ -2,6 +2,8 @@
 
 namespace Pdsinterop\Authentication;
 
+use Exception;
+use League\OAuth2\Server\Exception\OAuthServerException;
 use Psr\Http\Message\ResponseInterface;
 
 abstract class AbstractRouter
@@ -23,6 +25,21 @@ abstract class AbstractRouter
     public function __construct(ResponseInterface $response)
     {
         $this->response = $response;
+    }
+
+    public static function createExceptionResponse(
+        Exception $exception,
+        ResponseInterface $response
+    ) : ResponseInterface {
+        $response = $response->withStatus(500);
+        $response->getBody()->write($exception);
+
+        if ($exception instanceof OAuthServerException) {
+            // All instances of OAuthServerException can be formatted into a HTTP response
+            $response = $exception->generateHttpResponse($response);
+        }
+
+        return $response;
     }
 
     abstract public function route() : callable;
