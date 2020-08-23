17ea393b6eb59239a7ffec0ff8bfbee2e339c5de
Ben Peachey <potherca@gmail.com>
2020-08-23 14:42:26 +0200

    Add filesystem and support for public files to Resource Handler.

--- src/Resource/Handler/Resource.php
+++ src/Resource/Handler/Resource.php
@@ -1,49 +1,90 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Pdsinterop\Authentication\Resource\Handler;
 
+use League\Flysystem\Filesystem;
 use Pdsinterop\Authentication\AbstractHandler;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Pdsinterop\Authentication\Enum\OAuth2\Parameter;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
+/**
+ * In order to visit a resource, a user needs to be authenticated.
+ *
+ * This can be done with any of the OAuth2 grant types supported by the server
+ * (listed under the `/client` URI).
+ *
+ * If the user is authorized, the authorization method/token/etc. are be shown.
+ *
+ * Otherwise the user is redirected to a screen asking them for authorization.
+ *
+ * The resource is returned as JSON.
+ */
 class Resource extends AbstractHandler
 {
-    //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    ////////////////////////////// CLASS PROPERTIES \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    private const KEY_AUTHENTICATE = 'authenticate';
+
+    /** @var Authentication */
+    private $authentication;
+    /** @var Filesystem */
+    private $filesystem;
+    /** @var string[] */
+    private $publicFiles;
+
+    //////////////////////////// GETTERS AND SETTERS \\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    private function getPublicFiles() : array
+    {
+        return $this->publicFiles;
+    }
 
     /**
-     * In order to visit this resource, a user needs to be authenticated.
-     *
-     * This can be done with any of the OAuth2 grant types supported by the
-     * server (listed under the `/client` URI).
-     *
-     * If the user is authorized, the authorization method/token/etc. should be shown.
-     *
-     * Otherwise the user is redirected to a screen asking them for authorization.
-     *
-     * The resource is returned as JSON.
-     *
-     * @param ServerRequestInterface $request
-     * @param array $args
-     *
-     * @return ResponseInterface
+     * @param string[] $publicFiles
      */
-    public function __invoke(ServerRequestInterface $request, array $args) : ResponseInterface
+    public function setPublicResources(array $publicFiles) : void
     {
+        $this->publicFiles = $publicFiles;
+    }
+
+    //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    public function __construct(Response $response, Filesystem $filesystem, Authentication $authentication)
+    {
+        $this->authentication = $authentication;
+        $this->filesystem = $filesystem;
+
+        parent::__construct($response);
+    }
+
+    public function __invoke(Request $request, array $args) : Response
+    {
+        $filesystem = $this->filesystem;
         $response = $this->getResponse();
 
-        $content = '';
+        $queryParams = $request->getQueryParams();
+        $path = $args['path'];
 
-        $isAuthorized = $this->isAuthorized();
+        $showAuthentication = array_key_exists(self::KEY_AUTHENTICATE, $queryParams);
+        $showFile = $this->isAuthorized() || $this->isPublicFile($path);
 
-        if ($isAuthorized) {
-            $content = '{"message": "Information about the resource owner goes here"}';
+        if ($showAuthentication) {
+            $authenticationHandler = $this->authentication;
+            $authenticationHandler($request, $args);
+        } elseif ($showFile) {
+            if ($filesystem->has($path)) {
+                $resource = $filesystem->read($path);
+                $response = $this->buildResourceResponse($resource);
+            } else {
+                $response = $this->buildNotExistResponse($request);
+            }
         } else {
-            $response = $response->withHeader('Location', rtrim($request->getRequestTarget(), '/') . '/authenticate')->withStatus(302);
+            $url = rtrim($request->getRequestTarget(), '/');
+            $response = $response->withHeader('Location', $url . '?' . self::KEY_AUTHENTICATE)->withStatus(302);
         }
 
-        $response->getBody()->write($content);
-
-        return $response->withHeader('content-type', 'application/json; charset=UTF-8');
+        return $response;
     }
 
     ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -51,5 +92,34 @@ class Resource extends AbstractHandler
     private function isAuthorized() : bool
     {
         return false;
+    }
+
+    private function isPublicFile($path) : bool
+    {
+        return in_array($path, $this->getPublicFiles(), true);
+    }
+
+    private function buildNotExistResponse(Request $request) : Response
+    {
+        $description = vsprintf(
+            'The requested resource "%s" does not exist on this server.',
+            [$request->getRequestTarget()]
+        );
+
+        $response = $this->createJsonResponse([
+            Parameter::ERROR => 'Not Found',
+            Parameter::ERROR_DESCRIPTION => $description,
+        ]);
+
+        return $response->withStatus(404);
+    }
+
+    private function buildResourceResponse(string $resource) : Response
+    {
+        $response = $this->createJsonResponse([
+            Parameter::RESOURCE => base64_encode($resource),
+        ]);
+
+        return $response->withHeader('content-type', 'application/json; charset=UTF-8');
     }
 }
