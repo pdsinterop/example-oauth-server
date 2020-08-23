9f94a9c920b09375cb4f3e0b0b454b6e51c908e6
Ben Peachey <potherca@gmail.com>
2020-08-23 14:50:38 +0200

    Change Resource Router handling of root requests.

--- src/Resource/Router.php
+++ src/Resource/Router.php
@@ -4,8 +4,9 @@ namespace Pdsinterop\Authentication\Resource;
 
 use League\Route\RouteGroup;
 use Pdsinterop\Authentication\AbstractRouter;
+use Pdsinterop\Authentication\RedirectWithSlashHandler as RedirectHandler;
 use Pdsinterop\Authentication\Resource\Handler\Authentication;
-use Pdsinterop\Authentication\Resource\Handler\Resource as ResourceHandler;
+use Psr\Http\Message\ResponseInterface as Response;
 
 class Router extends AbstractRouter
 {
@@ -15,9 +16,32 @@ class Router extends AbstractRouter
     {
         $response = $this->getResponse();
 
-        return static function (RouteGroup $router) use ($response) {
-            $router->map('GET', '', new ResourceHandler($response));
+        return function (RouteGroup $router) use ($response) {
+            $router->map('GET', '/', new RedirectHandler($response));
+            $router->map('GET', '', $this->handleRootRequest($response));
             $router->map('GET', '/authenticate', new Authentication($response));
+        };
+    }
+
+    ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    private function handleRootRequest(Response $response) : callable
+    {
+        return static function (/*Request $request, array $args*/) use ($response) {
+            /** @noinspection HtmlUnknownTarget */
+            $response->getBody()->write(<<<'HTML'
+<h1>Available resources</h1>
+
+<p>The following resources are available:</p>
+
+<ul>
+    <li>a <a href="./private_resource.txt">private resource</a> that requires authentication</li>
+    <li>a <a href="./public_resource.txt">public resource</a> that can always be accessed</li>
+</ul>
+HTML
+            );
+
+            return $response;
         };
     }
 }
