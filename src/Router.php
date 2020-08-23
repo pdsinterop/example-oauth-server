28a33c010a1fb91fed3d507001c3c4cd47ce75a5
Ben Peachey <potherca@gmail.com>
2020-08-23 15:04:28 +0200

    Add Resource URL to main Router homepage view.

--- src/Router.php
+++ src/Router.php
@@ -3,6 +3,7 @@
 namespace Pdsinterop\Authentication;
 
 use League\Route\RouteGroup;
+use Pdsinterop\Authentication\Enum\ServerPrefix;
 use Psr\Http\Message\ResponseInterface;
 
 class Router extends AbstractRouter
@@ -23,14 +24,25 @@ class Router extends AbstractRouter
     private function homepageHandler(ResponseInterface $response) : callable
     {
         return static function (/*ServerRequestInterface $request, array $args*/) use ($response) {
-            $response->getBody()->write(<<<'HTML'
+            $prefix = [
+                'client' => ServerPrefix::CLIENT,
+                'resource' => ServerPrefix::RESOURCE,
+                'server' => ServerPrefix::AUTHORIZATION,
+            ];
+
+            $response->getBody()->write(<<<"HTML"
 
 <h1>Hello world!</h1>
+
+<p>
+    Some dummy resources have been added, to complete the example.
+</p>
 
 <p>The following URLs can be visited:</p>
 
 <ul>
     <li><a href="/">/</a> The page you find yourself on now.</li>
+    <li><a href="{$prefix['resource']}">{$prefix['resource']}</a> Example <strong>resources</strong> to trigger the authentication process using the server and client URLs.</li>
 </ul>
 
 HTML
