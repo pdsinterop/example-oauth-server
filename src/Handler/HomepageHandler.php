e78ab1a20429994f48283973b3c31d941dee428b
Ben Peachey <potherca@gmail.com>
2020-08-25 17:01:03 +0200

    Add homepage handler as separate class

---
+++ src/Handler/HomepageHandler.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace Pdsinterop\Authentication\Handler;
+
+use Pdsinterop\Authentication\AbstractHandler;
+use Pdsinterop\Authentication\Enum\ServerPrefix;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class HomepageHandler extends AbstractHandler
+{
+    public function __invoke(Request $request, array $args) : Response
+    {
+        $response = $this->getResponse();
+
+        $prefix = [
+            'client' => ServerPrefix::CLIENT,
+            'resource' => ServerPrefix::RESOURCE,
+            'server' => ServerPrefix::AUTHORIZATION,
+        ];
+
+        $response->getBody()->write(<<<"HTML"
+            <h1>Hello world!</h1>
+
+            <p>
+                Some dummy resources have been added, to complete the example.
+            </p>
+
+            <p>The following URLs can be visited:</p>
+
+            <ul>
+                <li><a href="/">/</a> The page you find yourself on now.</li>
+                <li>
+                    <a href="{$prefix['resource']}">{$prefix['resource']}</a>
+                    Example <strong>resources</strong> to trigger the
+                    authentication process using the server and client URLs.
+                </li>
+            </ul>
+HTML
+        );
+
+        return $response->withStatus(200);
+    }
+}
--- src/Router.php
+++ src/Router.php
@@ -3,8 +3,7 @@
 namespace Pdsinterop\Authentication;
 
 use League\Route\RouteGroup;
-use Pdsinterop\Authentication\Enum\ServerPrefix;
-use Psr\Http\Message\ResponseInterface;
+use Pdsinterop\Authentication\Handler\HomepageHandler;
 
 class Router extends AbstractRouter
 {
@@ -12,43 +11,12 @@ class Router extends AbstractRouter
 
     final public function route() : callable
     {
-        return function (RouteGroup $router) {
-            $response = $this->getResponse();
+        $response = $this->getResponse();
 
-            $router->map('GET', '/', $this->homepageHandler($response));
+        return static function (RouteGroup $router) use ($response) {
+            $router->map('GET', '/', new HomepageHandler($response));
         };
     }
 
     ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-    private function homepageHandler(ResponseInterface $response) : callable
-    {
-        return static function (/*ServerRequestInterface $request, array $args*/) use ($response) {
-            $prefix = [
-                'client' => ServerPrefix::CLIENT,
-                'resource' => ServerPrefix::RESOURCE,
-                'server' => ServerPrefix::AUTHORIZATION,
-            ];
-
-            $response->getBody()->write(<<<"HTML"
-
-<h1>Hello world!</h1>
-
-<p>
-    Some dummy resources have been added, to complete the example.
-</p>
-
-<p>The following URLs can be visited:</p>
-
-<ul>
-    <li><a href="/">/</a> The page you find yourself on now.</li>
-    <li><a href="{$prefix['resource']}">{$prefix['resource']}</a> Example <strong>resources</strong> to trigger the authentication process using the server and client URLs.</li>
-</ul>
-
-HTML
-            );
-
-            return $response->withStatus(200);
-        };
-    }
 }
