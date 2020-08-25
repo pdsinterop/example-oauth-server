0a51c2423dec8a528216a692c697a8e2bcf08853
Ben Peachey <potherca@gmail.com>
2020-08-25 17:33:13 +0200

    Add Enum class to represent form/post fields.

---
+++ src/Client/Enum/Field.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Pdsinterop\Authentication\Client\Enum;
+
+class Field
+{
+    public const USERNAME = 'username';
+    public const PASSWORD = 'password';
+}
