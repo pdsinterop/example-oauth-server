7220eeb26b39257f9bd640204afb10ef5e29cf28
Ben Peachey <potherca@gmail.com>
2020-09-04 12:20:03 +0200

    Change Server router to have cleaner code
    - Replace interface class names with aliases
    - Remove unused use statement
    - Add homepage content

--- src/Server/Router.php
+++ src/Server/Router.php
@@ -2,15 +2,15 @@
 
 namespace Pdsinterop\Authentication\Server;
 
-use Exception;
 use League\OAuth2\Server\AuthorizationServer;
-use League\OAuth2\Server\Entities\UserEntityInterface;
+use League\OAuth2\Server\Entities\UserEntityInterface as User;
 use League\Route\RouteGroup;
 use Pdsinterop\Authentication\AbstractRouter;
 use Pdsinterop\Authentication\Enum\Authorization;
-use Pdsinterop\Authentication\Server\Factory\AuthorizationServerFactory;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Pdsinterop\Authentication\RedirectWithSlashHandler as RedirectHandler;
+use Pdsinterop\Authentication\Server\Factory\AuthorizationServerFactory as ServerFactoryAlias;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 class Router extends AbstractRouter
 {
@@ -18,17 +18,17 @@ class Router extends AbstractRouter
 
     /** @var array */
     private $grantTypes;
-    /** @var AuthorizationServerFactory */
+    /** @var ServerFactoryAlias */
     private $serverFactory;
-    /** @var UserEntityInterface */
+    /** @var User */
     private $user;
 
     //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
     final public function __construct(
-        ResponseInterface $response,
-        AuthorizationServerFactory $serverFactory,
-        UserEntityInterface $user,
+        Response $response,
+        ServerFactoryAlias $serverFactory,
+        User $user,
         array $grantTypes
     ) {
         parent::__construct($response);
@@ -50,29 +50,29 @@ class Router extends AbstractRouter
 
         // @TODO: The definitions $router->map() calls should stay here but the methods should (probably?) be moved to another class
         return function (RouteGroup $router) use ($response, $server, $user) {
-            $router->map('GET', '/authorize', $this->routeAuthorizationCodeGrant($response, $server, $user));
-            $router->map('POST', '/authorize', $this->routeAuthorizationCodeGrant($response, $server, $user));
+            $router->map('GET', '/', new RedirectHandler($response));
+            $router->map('GET', '', $this->handleHomePage($response));
 
-            $router->map('GET', '/access_token', $this->accessTokenHandler($response, $server));
+            $router->map('POST', '/authorize', $this->routeAuthorizationCodeGrant($response, $server, $user));
             $router->map('POST', '/access_token', $this->accessTokenHandler($response, $server));
         };
     }
 
     ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-    private function accessTokenHandler(ResponseInterface $response, AuthorizationServer $server) : callable
+    private function accessTokenHandler(Response $response, AuthorizationServer $server) : callable
     {
-        return static function (ServerRequestInterface $request/*, array $args*/) use ($response, $server) {
+        return static function (Request $request/*, array $args*/) use ($response, $server) {
             return $server->respondToAccessTokenRequest($request, $response);
         };
     }
 
     private function routeAuthorizationCodeGrant(
-        ResponseInterface $response,
+        Response $response,
         AuthorizationServer $server,
-        UserEntityInterface $user
+        User $user
     ) : callable {
-        return static function (ServerRequestInterface $request/*, array $args*/) use ($response, $server, $user) {
+        return static function (Request $request/*, array $args*/) use ($response, $server, $user) {
             // Validate the HTTP request and return an AuthorizationRequest object.
             $authRequest = $server->validateAuthorizationRequest($request);
 
@@ -90,6 +90,32 @@ class Router extends AbstractRouter
 
             // Return the HTTP redirect response
             $response = $server->completeAuthorizationRequest($authRequest, $response);
+
+            return $response;
+        };
+    }
+
+    private function handleHomePage(Response $response) : callable
+    {
+        return static function (/*ServerRequestInterface $request, array $args*/) use ($response) {
+            $response->getBody()->write(<<<"HTML"
+                    <h1>Server URLs</h1>
+
+                    <p>In OAuth2, there are several ways to authenticate a client against a server.</p>
+
+                    <p>The end result of authentication is always that an access token is obtained by the client.</p>
+
+                    <p>The mechanism used to acquiring an access token is called a "grant"</p>
+
+                    <p>Several grants are available, they can be visited at the following URLs:</p>
+
+                    <ul>
+                        <li><a href="./authorize">/authorize</a></li>
+                        <li><a href="./access_token">/access_token</a></li>
+                    </ul>
+HTML
+            );
+
             return $response;
         };
     }
